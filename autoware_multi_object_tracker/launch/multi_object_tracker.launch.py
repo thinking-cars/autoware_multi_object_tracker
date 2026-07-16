36e158a7a9300fad52b1f1f11b3aea881aec4649
Raphael van Kempen <vankempen@thinking-cars.de>
2026-07-16 06:45:46 +0000

    fix topic remappings and channel params

--- autoware_multi_object_tracker/launch/multi_object_tracker.launch.py
+++ autoware_multi_object_tracker/launch/multi_object_tracker.launch.py
@@ -20,24 +20,37 @@ def generate_launch_description():
     package_share = get_package_share_directory("autoware_multi_object_tracker")
 
     remappable_topics = [
-        DeclareLaunchArgument("input_object_list_topic_01", default_value="~/input/detection01/objects"),
-        DeclareLaunchArgument("input_object_list_topic_02", default_value="~/input/detection02/objects"),
-        DeclareLaunchArgument("input_object_list_topic_03", default_value="~/input/detection03/objects"),
-        DeclareLaunchArgument("input_object_list_topic_04", default_value="~/input/detection04/objects"),
-        DeclareLaunchArgument("input_object_list_topic_05", default_value="~/input/detection05/objects"),
-        DeclareLaunchArgument("input_object_list_topic_06", default_value="~/input/detection06/objects"),
-        DeclareLaunchArgument("input_object_list_topic_07", default_value="~/input/detection07/objects"),
-        DeclareLaunchArgument("input_object_list_topic_08", default_value="~/input/detection08/objects"),
-        DeclareLaunchArgument("input_object_list_topic_09", default_value="~/input/detection09/objects"),
-        DeclareLaunchArgument("input_object_list_topic_10", default_value="~/input/detection10/objects"),
-        DeclareLaunchArgument("input_object_list_topic_11", default_value="~/input/detection11/objects"),
-        DeclareLaunchArgument("input_object_list_topic_12", default_value="~/input/detection12/objects"),
-        DeclareLaunchArgument("odometry_topic", default_value="~/input/odometry"),
-        DeclareLaunchArgument("objects_topic", default_value="~/output/objects"),
-        DeclareLaunchArgument("merged_objects_topic", default_value="~/output/merged_objects"),
+        DeclareLaunchArgument("input/detection01/objects", default_value="~/input/detection01/objects"),
+        DeclareLaunchArgument("input/detection02/objects", default_value="~/input/detection02/objects"),
+        DeclareLaunchArgument("input/detection03/objects", default_value="~/input/detection03/objects"),
+        DeclareLaunchArgument("input/detection04/objects", default_value="~/input/detection04/objects"),
+        DeclareLaunchArgument("input/detection05/objects", default_value="~/input/detection05/objects"),
+        DeclareLaunchArgument("input/detection06/objects", default_value="~/input/detection06/objects"),
+        DeclareLaunchArgument("input/detection07/objects", default_value="~/input/detection07/objects"),
+        DeclareLaunchArgument("input/detection08/objects", default_value="~/input/detection08/objects"),
+        DeclareLaunchArgument("input/detection09/objects", default_value="~/input/detection09/objects"),
+        DeclareLaunchArgument("input/detection10/objects", default_value="~/input/detection10/objects"),
+        DeclareLaunchArgument("input/detection11/objects", default_value="~/input/detection11/objects"),
+        DeclareLaunchArgument("input/detection12/objects", default_value="~/input/detection12/objects"),
+        DeclareLaunchArgument("input/odometry", default_value="~/input/odometry"),
+        DeclareLaunchArgument("output/objects", default_value="~/output/objects"),
+        DeclareLaunchArgument("output/merged_objects", default_value="~/output/merged_objects"),
     ]
 
-    channel_arguments = [f"input/detection{index:02d}/channel" for index in range(1, 13)]
+    channel_arguments = [
+        DeclareLaunchArgument("input/detection01/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection02/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection03/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection04/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection05/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection06/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection07/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection08/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection09/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection10/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection11/channel", default_value="none"),
+        DeclareLaunchArgument("input/detection12/channel", default_value="none"),
+    ]
 
     args = [
         DeclareLaunchArgument("name", default_value="autoware_multi_object_tracker", description="node name"),
@@ -48,7 +61,7 @@ def generate_launch_description():
             description="ROS logging level (debug, info, warn, error, fatal)",
         ),
         *remappable_topics,
-        *[DeclareLaunchArgument(name, default_value="none") for name in channel_arguments],
+        *channel_arguments,
         DeclareLaunchArgument(
             "tracker_setting_path",
             default_value=os.path.join(package_share, "config", "multi_object_tracker_node.param.yaml"),
@@ -90,12 +103,12 @@ def generate_launch_description():
             LaunchConfiguration("data_association_matrix_path"),
             LaunchConfiguration("input_channels_path"),
             {
-                **{name: LaunchConfiguration(name) for name in channel_arguments},
                 "publish_merged_objects": ParameterValue(
                     LaunchConfiguration("publish_merged_objects"), value_type=bool
                 ),
                 "ego_source": LaunchConfiguration("ego_source"),
             },
+            {argument.name: LaunchConfiguration(argument.name) for argument in channel_arguments},
         ],
         arguments=["--ros-args", "--log-level", LaunchConfiguration("log_level")],
         remappings=[(f"~/{argument.name}", LaunchConfiguration(argument.name)) for argument in remappable_topics],
